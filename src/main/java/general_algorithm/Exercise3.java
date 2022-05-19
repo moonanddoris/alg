94e42edd5a484908899a8a33cf480efa063fc429
lixinhai <xinhai.li@yiducloud.cn>
2022-05-19 10:21:21 +0800

    动态规划背包问题

--- src/main/java/general_algorithm/Exercise3.java
+++ src/main/java/general_algorithm/Exercise3.java
@@ -9,12 +9,17 @@ import java.util.Arrays;
  */
 public class Exercise3 {
 
-    // 给定最大重量限制值
-
+    /**
+     * 0-1背包问题。
+     * 给定一组不可分割的物品，和一个载重限制limitVal的背包。在不超过最大载重限制的前提下，背包内最大重量是多大？
+     * @param limitVal
+     * @param staffs
+     */
     public static void knapsack(int limitVal, int[] staffs){
 
         int[][] st = new int[staffs.length][limitVal+1];
 
+        // 初始化
         for (int i=0; i< staffs.length; i++){
             for(int j =0; j< limitVal+1; j++){
                 st[i][j] = 0;
@@ -60,7 +65,7 @@ public class Exercise3 {
     }
 
     /**
-     * 优化二维数组
+     * 优化二维数组，使用一维数据即可完成
      * @param limitVal
      * @param staffs
      */
@@ -90,16 +95,59 @@ public class Exercise3 {
                 }
             }
         }
+        System.out.println("--------------------"+ Arrays.toString(st));
     }
 
-    //TODO 加入物品的价值
+    /**
+     * 加入物品价值
+     * @param limitVal
+     * @param staffs
+     */
+    public static void knapsackPro2(int limitVal, int[] staffs, int[] staffsValue){
+
+        int[] st = new int[limitVal+1];
+
+        for(int i=0; i<limitVal+1; i++){
+            st[i] = -1;
+        }
+
+        for(int i=0; i< staffs.length; i++){
+            System.out.println("--------------------"+ Arrays.toString(st));
+            if(i == 0){
+                st[0] = 0;
+                if(staffs[i] <= limitVal) {
+                    st[staffs[i]] = staffs[i] * staffsValue[i];
+                }
+            }
+            else {
+                // 避免重复赋值的问题，从后向前遍历
+                for(int j=limitVal; j>=0; j--){
+                    if(st[j] == -1){
+                        continue;
+                    }
+
+                    if(j + staffs[i] <= limitVal) {
+                        //st[j + staffs[i]] = 1;
+                        if(st[j] + staffs[i] * staffsValue[i] > st[j+staffs[i]]) {
+                            st[j + staffs[i]] = st[j] + staffs[i] * staffsValue[i];
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("--------------------"+ Arrays.toString(st));
+    }
 
     public static void main(String[] args){
 
         int limitVal = 10;
         int[] staff = {1, 1, 2, 3, 4, 5};
+        int[] staffValue = {10, 10, 4, 6, 7, 1};
 
-        knapsack(10, staff);
+        //knapsack(10, staff);
+        knapsackPro(10, staff);
+        knapsackPro2(10, staff, staffValue);
 
         System.out.println("----------------");
     }
